2eca3d37c27200819c0be6ac0896364ffc9afc76
GuptaJiPK <prateekgupta236@gmail.com>
2021-07-27 15:03:11 +0530

    First Angular Day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatatypeComponent } from './datatype/datatype.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
+import {FormsModule} from '@angular/forms';
 
+//decorator:<etadat information
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [//contains the list of component class
+    AppComponent,
+    DatatypeComponent,
+    DirectivesComponent,
+    TwowaybindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
